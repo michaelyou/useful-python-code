8904514a80c2bd1f101581433ed6c1bcc636bff0
michaelyou <michaelseu2011@gmail.com>
2015-06-13 13:45:18 +0800

    const

---
+++ const/const.py
@@ -0,0 +1,16 @@
+class const(object):
+	class ConstError(TypeError): 
+		pass
+	class ConstCaseError(ConstError):
+		pass
+
+	def __setattr__(self, name, value):
+		if self.__dict__.has_key(name):
+			raise self.ConstError, "can't change const.%s" % name
+		if not name.isupper():
+			raise self.ConstCaseError, "const name '%s' is not all uppercase" % name
+
+		self.__dict__[name] = value
+
+import sys
+sys.modules[__name__] = const()
